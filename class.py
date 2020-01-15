aab86bb7f34b3a58c0df51a8689ed8b2f8ad24cc
zealot <123@asd.ru>
2020-01-15 22:54:11 +0300

    work in progress

--- class.py
+++ class.py
@@ -1,61 +1,95 @@
 import os.path
-from git import *
+#from git import *
 import git, os, shutil
 
 # create local Repo/Folder
-###UPLOAD_FOLDER = "F:\zealot"
-UPLOAD_FOLDER = "F:\python\python_test1"
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-    print(UPLOAD_FOLDER)
+if os.name == 'nt':
+    UPLOAD_FOLDER = "%USERPROFILE%\Desktop\python1_test"
+    #UPLOAD_FOLDER = "C:\python_test"
+else:
+    UPLOAD_FOLDER = "~/python1_test"
+
+#if not os.path.exists(UPLOAD_FOLDER):
+#    os.makedirs(UPLOAD_FOLDER)
+#    print(UPLOAD_FOLDER)
 new_path = os.path.join(UPLOAD_FOLDER)
 DIR_NAME = new_path
 REMOTE_URL = "https://github.com/Darth-Kordis/python_test.git"
-#REMOTE_URL = "GitURL"  # if you already connected with server you dont need to give any credential
-# REMOTE_URL looks "git@github.com:path of Repo"
-# code for clone
+NEW_BRANCH: str = 'another-new-one'
 
-
-class git_operation_clone():
+class git_operations():
     try:
         def __init__(self):
             self.DIR_NAME = DIR_NAME
             self.REMOTE_URL = REMOTE_URL
+            self.NEW_BRANCH = NEW_BRANCH
 
         def git_clone(self):
             if not os.path.exists(DIR_NAME):
                 if os.path.isdir(DIR_NAME):
                     shutil.rmtree(DIR_NAME)
                 os.mkdir(DIR_NAME)
-            repo = git.Repo.init(DIR_NAME)
-            origin = repo.create_remote('origin', REMOTE_URL)
-            origin.fetch()
-            origin.pull(origin.refs[0].remote_head)
+            print(DIR_NAME)
+            print()
+            if not os.listdir(DIR_NAME):
+                repo = git.Repo.init(DIR_NAME)
+                origin = repo.create_remote('origin', REMOTE_URL)
+                origin.fetch()
+                origin.pull(origin.refs[0].remote_head)
+            print("repo cloned successfully")
+            print()
+
+        def git_branch(self):
+            try:
+                repo = git.Repo(DIR_NAME)
+                if NEW_BRANCH not in repo.heads:
+                    repo.git.branch(NEW_BRANCH)
+                    print('branch "' + NEW_BRANCH + '" created successfully')
+                else:
+                    print('branch "' + NEW_BRANCH + '" is already exist')
+                print("available branches:")
+                print(repo.heads)
+                print()
+            except Exception as e:
+                print(str(e))
+
+        def git_checkout(self):
+            try:
+                repo = git.Repo(DIR_NAME)
+                repo.git.checkout(NEW_BRANCH)
+                branch = repo.active_branch
+                print("active branch is: " + branch.name)
+            except Exception as e:
+                print(str(e))
+
+
+        def git_commit(self):
+            try:
+                repo = git.Repo(DIR_NAME)
+                commit_message = 'work in progress'
+                repo.git.add('--all')
+                repo.index.commit(commit_message)
+            except Exception as e:
+                print(str(e))
+
+        def git_push(self):
+            try:
+                repo = git.Repo(DIR_NAME)
+                origin = repo.remote('origin')
+                branch = repo.active_branch
+                origin.push(branch.name)
+                repo.git.add(update=True)
+                print("repo push successfully")
+            except Exception as e:
+                print(str(e))
+
     except Exception as e:
         print(str(e))
 
-
-# code for push
-class git_operation_push():
-    def git_push_file(self):
-        try:
-            repo = Repo(DIR_NAME)
-            commit_message = 'work in progress'
-            # repo.index.add(u=True)
-            repo.git.add('--all')
-            repo.index.commit(commit_message)
-            origin = repo.remote('origin')
-            origin.push('master')
-            repo.git.add(update=True)
-            print("repo push succesfully")
-        except Exception as e:
-            print(str(e))
-
-
 if __name__ == '__main__':
-    a = git_operation_push()
-
-    git_operation_clone()
-    git_operation_clone.git_clone('')
-    git_operation_push.git_push_file('')
-    ###
+    git_operations()
+    git_operations.git_clone('')
+    git_operations.git_branch('')
+    git_operations.git_checkout('')
+    git_operations.git_commit('')
+    git_operations.git_push('')
