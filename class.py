01f29e9337a5761fb68285ef4c1c5e86981bcbb3
zealot <123@asd.ru>
2020-01-16 01:19:11 +0300

    work in progress

--- class.py
+++ class.py
@@ -1,91 +1,95 @@
-import os.path
-import git, os, shutil
+import os
+#os.system('pip3 install gitpython')
+
+import git, shutil
 
 # create local Repo/Folder
 if os.name == 'nt':
-    UPLOAD_FOLDER = "%USERPROFILE%\Desktop\python1_test"
+    UPLOAD_FOLDER = os.environ['OLDPWD'] + '%USERPROFILE%\Desktop\python1_test'
     #UPLOAD_FOLDER = "C:\python_test"
 else:
-    UPLOAD_FOLDER = "~/python1_test"
+    UPLOAD_FOLDER = os.environ['OLDPWD'] + '/python1_test'
 
 new_path = os.path.join(UPLOAD_FOLDER)
-DIR_NAME = new_path
-REMOTE_URL = "https://github.com/Darth-Kordis/python_test.git"
-NEW_BRANCH: str = 'another-new-one'
+
+#print (new_path)
+
+NAME = new_path
+URL = 'https://github.com/Darth-Kordis/python_test.git'
+BRANCH: str = 'another-new-one'
 
 class git_operations():
-    try:
-        def __init__(self):
-            self.DIR_NAME = DIR_NAME
-            self.REMOTE_URL = REMOTE_URL
-            self.NEW_BRANCH = NEW_BRANCH
+    def __init__(self, name, url, branch):
+        self.DIR_NAME = name
+        self.REMOTE_URL = url
+        self.NEW_BRANCH = branch
 
-        def git_clone(self):
-            if not os.path.exists(DIR_NAME):
-                if os.path.isdir(DIR_NAME):
-                    shutil.rmtree(DIR_NAME)
-                os.mkdir(DIR_NAME)
-            print(DIR_NAME)
-            print()
-            if not os.listdir(DIR_NAME):
-                repo = git.Repo.init(DIR_NAME)
-                origin = repo.create_remote('origin', REMOTE_URL)
+    def git_clone(self):
+        try:
+            if not os.path.exists(self.DIR_NAME):
+                if os.path.isdir(self.DIR_NAME):
+                    shutil.rmtree(self.DIR_NAME)
+                os.mkdir(self.DIR_NAME)
+                print(self.DIR_NAME, '\n\r')
+
+            if not os.listdir(self.DIR_NAME):
+                repo = git.Repo.init(self.DIR_NAME)
+                origin = repo.create_remote('origin', self.REMOTE_URL)
                 origin.fetch()
                 origin.pull(origin.refs[0].remote_head)
             print("repo cloned successfully")
             print()
+        except Exception as e:
+            print(str(e))
 
-        def git_branch(self):
-            try:
-                repo = git.Repo(DIR_NAME)
-                if NEW_BRANCH not in repo.heads:
-                    repo.git.branch(NEW_BRANCH)
-                    print('branch "' + NEW_BRANCH + '" created successfully')
-                else:
-                    print('branch "' + NEW_BRANCH + '" is already exist')
-                print("available branches:")
-                print(repo.heads)
-                print()
-            except Exception as e:
-                print(str(e))
+    def git_branch(self):
+        try:
+            repo = git.Repo(self.DIR_NAME)
+            if self.NEW_BRANCH not in repo.heads:
+                repo.git.branch(self.NEW_BRANCH)
+                print('branch "' + self.NEW_BRANCH + '" created successfully')
+            else:
+                print('branch "' + self.NEW_BRANCH + '" is already exist')
+            print("available branches:")
+            print(repo.heads)
+            print()
+        except Exception as e:
+            print(str(e))
 
-        def git_checkout(self):
-            try:
-                repo = git.Repo(DIR_NAME)
-                repo.git.checkout(NEW_BRANCH)
-                branch = repo.active_branch
-                print("active branch is: " + branch.name)
-            except Exception as e:
-                print(str(e))
+    def git_checkout(self):
+        try:
+            repo = git.Repo(self.DIR_NAME)
+            repo.git.checkout(self.NEW_BRANCH)
+            branch = repo.active_branch
+            print("active branch is: " + branch.name)
+        except Exception as e:
+            print(str(e))
 
 
-        def git_commit(self):
-            try:
-                repo = git.Repo(DIR_NAME)
-                commit_message = 'work in progress'
-                repo.git.add('--all')
-                repo.index.commit(commit_message)
-            except Exception as e:
-                print(str(e))
+    def git_commit(self):
+        try:
+            repo = git.Repo(self.DIR_NAME)
+            commit_message = 'work in progress'
+            repo.git.add('--all')
+            repo.index.commit(commit_message)
+        except Exception as e:
+            print(str(e))
 
-        def git_push(self):
-            try:
-                repo = git.Repo(DIR_NAME)
-                origin = repo.remote('origin')
-                branch = repo.active_branch
-                origin.push(branch.name)
-                repo.git.add(update=True)
-                print("repo push successfully")
-            except Exception as e:
-                print(str(e))
-
-    except Exception as e:
-        print(str(e))
+    def git_push(self):
+        try:
+            repo = git.Repo(self.DIR_NAME)
+            origin = repo.remote('origin')
+            branch = repo.active_branch
+            origin.push(branch.name)
+            repo.git.add(update=True)
+            print("repo push successfully")
+        except Exception as e:
+            print(str(e))
 
 if __name__ == '__main__':
-    git_operations()
-    git_operations.git_clone('')
-    git_operations.git_branch('')
-    git_operations.git_checkout('')
-    git_operations.git_commit('')
-    git_operations.git_push('')
+    a = git_operations(NAME, URL, BRANCH)
+    a.git_clone()
+    a.git_branch()
+    a.git_checkout()
+    a.git_commit()
+    a.git_push()
